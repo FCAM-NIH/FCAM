873c573592405c5715e22ae02873c948108efc48
Marinelli <marinellif@mia.nhlbi.nih.gov>
2021-11-01 15:45:07 -0400

    Adde combine on diala and clearup of directories

--- tutorial/plumed1.3/diala-be/individual_plus_combine/run.sh
+++ tutorial/plumed1.3/diala-be/individual_plus_combine/run.sh
@@ -6,6 +6,8 @@ python3.8 ../../../../calcf_vgauss.py -if input.dat --justcalceffpoint > job&
 
 python3.8 ../../../../calcf_vgauss.py -if input_combine.dat
 
+# calculate free energy
+
 python3.8 ../../../graf_fes_kmc.py -ff grad_on_eff_points.out -units kj -temp 1298 -nsteps 10000000000 -weth 1 -ofesf fes.out
 
 # evaluate error from first and second block
