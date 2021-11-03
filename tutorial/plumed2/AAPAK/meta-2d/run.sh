783a9fd26d492f52c45054bd4fc23146841cd9cb
Marinelli <marinellif@mia.nhlbi.nih.gov>
2021-11-03 14:53:46 -0400

    Added tutorial on AAPAK and AWPAK peptides for plumed2

---
+++ tutorial/plumed2/AAPAK/meta-2d/run.sh
@@ -0,0 +1,10 @@
+python3.8 ../../../../calcf_vgauss.py -if input.dat -units kj -temp 300 -calcmetaf -colv_time_prec 2 > job&
+
+python3.8 ../../../../graf_fes_kmc.py -ff grad_on_eff_points.out -units kj -temp 2300 -nsteps 10000000000 -weth 1 -ofesf fes.out
+
+# evaluate error from first and second block
+
+python3.8 ../../../../graf_fes_kmc.py -ff grad_on_eff_points1.out -units kj -temp 2300 -nsteps 10000000000 -weth 1 -ofesf fes1.out
+
+python3.8 ../../../../graf_fes_kmc.py -ff grad_on_eff_points2.out -units kj -temp 2300 -nsteps 10000000000 -weth 1 -ofesf fes2.out
+
