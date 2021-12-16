5694cfec93c9826c894491f33f2d5064c77e67e1
fabsugar <fabrizio.marinelli@gmail.com>
2021-12-16 11:29:26 -0500

    Modified tutorial/colvars/buta_2d_concurrent/partition_neigh based on correction to do_partitions.sh script. Verified that test work

--- tutorial/colvars/buta_2d_concurrent/partition_neighs/run.sh
+++ tutorial/colvars/buta_2d_concurrent/partition_neighs/run.sh
@@ -2,7 +2,9 @@
 cp /data/TMB-CSB/Marinelli/PROG/FCAM/tools/partition_neighs/do_partitions.sh .
 cp /data/TMB-CSB/Marinelli/PROG/FCAM/tools/partition_neighs/job_tmp.sh .
 
-bash do_partitions.sh ../results/eff_points.out ../results/grad_on_eff_points.out /data/TMB-CSB/Marinelli/PROG/FCAM/graf_fes_kmc.py
+# the following divides the size of the grid point file by 7. The run entials 28 (4x7) jobs based on 28 overlapping partitions
+
+bash do_partitions.sh ../results/eff_points.out ../results/grad_on_eff_points.out ~/PROG/FCAM/graf_fes_kmc.py 7
 
 python3.8 ../../../../graf_fes_kmc.py -ff ../results/grad_on_eff_points.out -units kcal -temp 1298 -nsteps 10000000000 -weth 1 -rneighs -ineighf neighsc_tot.out -nopneighs -ofesf  fes.out
 
