359e9195447ba2693fa2b48eb9d7929f9880d3a2
Marinelli <marinellif@mia.nhlbi.nih.gov>
2022-06-30 13:41:48 -0400

    tutorial zipped

--- tutorial/colvars/deca-L-alanine/Common/delete_tmp_files.tcl
+++
@@ -1,5 +0,0 @@
-if { ${mol_name} == "da" } {
-    file delete -force "index.ndx"
-    file delete -force "rmsd_atoms_refpos.xyz"
-    file delete -force "heavy_atoms_refpos.xyz"
-}
--- tutorial/plumed2/AWPAK/be/meta_inp/run.sh
+++
@@ -1,2 +0,0 @@
-mpirun -np 4 gmx_mpi mdrun -deffnm metad-be  -multi 2 -replex 1000 -plumed plumed -nsteps 10000000 >& job&
-
